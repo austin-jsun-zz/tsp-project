87b0e1050b4863def577259af9cab58f2a0033e0
Austin Sun <austin.jsun@gmail.com>
2019-11-19 22:10:13 -0800

    First commit

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    int numLocations;
+    int numHomes;
+    HashSet<String> locationNames;
+    HashSet<String> homeNames;
+    String startLocation;
+    
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
