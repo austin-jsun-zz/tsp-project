8484a552e1f6faf5e39e394ea573c5fa91e5a745
David Shau <davidshau22@berkeley.edu>
2019-11-19 22:51:41 -0800

    made adj_mat symettric

--- generator.py
+++ generator.py
@@ -5,9 +5,17 @@ def generate(n, edge_weights):
     Generates an adj matrix that represents a fully connected graph
     using only weights in edge_weights.
     """
-    g = []
-    for _ in range(n):
-        row = [random.choice(edge_weights) for j in range(n)]
-        g.append(row)
+    g = [['x'] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(i):
+            weight = random.choice(edge_weights)
+            g[i][j] = weight
+            g[j][i] = weight
     return g
-print(generate(3, edge_weights))
+
+def print_mat(mat):
+    for row in mat:
+        print(str(row))
+
+adj_mat = generate(3, edge_weights)
+print_mat(adj_mat)
