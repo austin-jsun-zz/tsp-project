769c3b478854d4a851202d07eb00cb08da967388
Austin Sun <austin.jsun@gmail.com>
2019-11-28 09:48:05 -0800

    Wrote functions for generating dropoff and indegree outdegree constraints, skeleton code for all other functions

--- lp_file_generator.py
+++ lp_file_generator.py
@@ -24,18 +24,121 @@ def generate_edge_list(number_of_locations, adjacency_matrix):
     return edge_list
 
 """
+This function maps input locations to an index in the order of their listing
+in list_of_locations.
+"""
+def generate_index_map(list_of_locations, number_of_locations):
+    index_map = {}
+    for i in range(0, number_of_locations):
+        index_map[list_of_locations[i]] = i
+    
+    return index_map
+
+"""
 This function generates the list of clh variables. clh = 1 if we drop off TA 
 living at home h at location l, and clh = 0 if we do not drop TA h at l
 """
 def generate_clh_list(list_of_locations, list_of_houses, number_of_locations):
     clh_list = []
-    index_map = {}
-    for i in range(0, number_of_locations):
-        index_map[list_of_locations[i]] = i
-        
+    index_map = generate_index_map(list_of_locations, number_of_locations)
+
     for i in range(0, number_of_locations):
         for h in list_of_houses:
             clh_list.append("c" + str(i) + str(index_map[h]))
 
     return clh_list
+
+"""
+This function generates the objective function string that we are attempting
+to minimize using LP. 
+"""
+def generate_objective_function():
+
+    return
+
+"""
+This function generates the constraint string that the sum of all clh 
+variables for each h is equal to 1, that is, the TA is only dropped off once.
+"""
+def generate_clh_dropoff_constraints(list_of_locations, list_of_houses, number_of_locations):
+    clh_dropoff_constraints_string = ""
+    index_map = generate_index_map(list_of_locations, number_of_locations)
+    for h in list_of_houses:
+        h_index = str(index_map[h])
+        for i in range(0, number_of_locations):
+            clh_dropoff_constraints_string += "c" + str(i) + str(h_index) + " "
+        clh_dropoff_constraints_string += "= 1" + "\n"
+    
+    return clh_dropoff_constraints_string
+
+"""
+This function generates the constraint string for each location in the graph,
+the indegree must equal the outdegree for any location to maintain the tour
+property.
+"""
+def generate_indegree_outdegree_constraints(list_of_locations, number_of_locations):
+    indegree_outdegree_constraint_string = ""
+    for i in range(0, number_of_locations):
+        i_index = str(i)
+        for j in range(0, number_of_locations):
+            indegree_outdegree_constraint_string += "x" + str(j) + i_index + " - " + "x" + i_index + str(j) + " "
+        indegree_outdegree_constraint_string += "= 0" + "\n"
+    
+    return indegree_outdegree_constraint_string
+
+"""
+This function generates constraints which ensure no subtours are selected,
+rather one single tour is selected. 
+"""
+def generate_no_subtours_constraints():
+
+    return
+
+"""
+This function generates valid dropoff constraints, essentially, the sum
+of indegrees to a vertex must be greater than the corresponding clh. So,
+if there is a positive indegree to the vertex, the corresponding clh can
+be one. If there is no positive indegree, the vertex is not included in
+our tour, and the corresponding clh must be 0 (we cannot dropoff at a vertex
+not included in our tour).
+"""
+def generate_valid_dropoff_constraints():
+
+    return 
+
+"""
+This function generates the source constraint which ensures that the source
+location is in our tour.
+"""
+def generate_source_constraint():
+
+    return
+
+"""
+This function generates the bounds of the program (sets xij for edges from i
+to j not in our graph to 0).
+"""
+def generate_bounds():
+
+    return 
+
+"""
+This function specifies which lp variables are restricted to take on the 
+values 0 or 1 - all the xij and all the clh.
+"""
+def generate_binary():
+
+    return 
+
+"""
+This function specifies which lp variables are restricted to take on 
+integer values - these are the u variables for elimination of subtour 
+constraint.
+"""
+def generate_integers():
+
+    return 
+
+
+
 
